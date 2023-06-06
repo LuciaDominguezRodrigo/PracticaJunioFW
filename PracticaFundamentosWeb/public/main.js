e58be71c1808d1d34a7121c9c6466c245f175f49
Lucia <117301166+LuciaDominguezRodrigo@users.noreply.github.com>
2023-06-06 18:41:55 +0200

    funciones main cambio de href y mensaje de borrar
    funciones de salto de página y de salto de notificación de borrar
    
    Co-Authored-By: Marcos Jiménez <117301291+MarJ03@users.noreply.github.com>

--- PracticaFundamentosWeb/public/main.js
+++ PracticaFundamentosWeb/public/main.js
@@ -1,7 +1,45 @@
-function cambiarVisibilidad(element) {
+export function cambiarVisibilidad(element) {
     if (element.style.display === "none") {
       element.style.display = "block";
     } else {
       element.style.display = "none";
     }
   }
+
+  function mensajeBorrar (nombrePlato){
+     let link = '/delete/' + nombrePlato;
+     let borrar= confirm ("¿quiere borrar el plato?");
+
+     if (borrar) {
+      window.location.href = link;
+     }
+  }
+
+  function masInfo (nombrePlato) {
+      let link = '/masInfo/' + nombrePlato;
+      window.location.href = link;
+  }
+
+  function forumlario () {
+     let link= '/formulario/';
+    window.location.href = link;
+  }
+
+
+  function cambiarIndice (){
+    let link = '/'
+    window.location.href = link;
+  }
+
+  function cambiarSobreNosotros (){
+    let link = 'sobrenosotros.html';
+    window.location.href=link;
+  }
+
+  function cambiarInicio(){
+    let link="inicio.html";
+    windowlocation.href=link;
+  }
+
+
+  
